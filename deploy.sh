42a5f2d7a1e8a050db721c5fe093796ab64dd41e
Moebe1 <syedmohsinparwez@connect.qut.edu.au>
2022-12-05 12:09:52 +1000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ bold=$(tput bold)
 echo -e
 echo -e "${bold}${green}Starting terraform apply with Auto Approve, in 5 seconds${reset}"
 echo -e
+sleep 5
 terraform apply -var-file aws_variables.tfvars --auto-approve &&
 echo -e
 echo -e "${green}Please wait, starting readiness check in 5 seconds${reset}"
